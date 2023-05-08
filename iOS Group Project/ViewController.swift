bb66ad0d2fd03b4ab3f9a2d746323769ab10d0f2
Jeejae <gavinjoshlogronio@gmail.com>
2023-05-09 00:28:52 +1000

    Testing

--- iOS Group Project/ViewController.swift
+++ iOS Group Project/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label1: UILabel!
+    @IBOutlet weak var bnt1: UIButton!
+    
+    var text:String = ""
     override func viewDidLoad() {
         super.viewDidLoad()
+        text = "Working"
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        
+        label1.text = text
+    }
+    
 }
 
