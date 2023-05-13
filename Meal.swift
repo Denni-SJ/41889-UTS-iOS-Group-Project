e9e0827b9504a24c29f567dbf1e0634f267519e1
Jeejae <gavinjoshlogronio@gmail.com>
2023-05-13 12:58:51 +1000

    meal class

--- Meal.swift
+++ Meal.swift
@@ -7,8 +7,24 @@
 
 import UIKit
 
-class Meal
-{
+class Meal{
+    var Name:String = ""
+    var Calories:Int = 0
+    var Protein: Int = 0
+    var Time: Int = 0
+    var Halal: Bool = false
+    var Carbs: Int = 0
+    var Gluten: Bool = false
+    var Ingredients: [String] = []
+    var Image: [UIImage] = []
     
+    func getName(name: String) -> String
+    {
+        return name
+    }
     
+    func getCalories()
+    {
+        
+    }
 }
