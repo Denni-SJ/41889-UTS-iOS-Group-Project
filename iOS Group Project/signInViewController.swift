7b011f10813a0f6deae10541b9106c476d444e09
Kyra Tsang <kyraatsang@gmail.com>
2023-05-20 20:39:52 +1000

    sign up & sign in

--- iOS Group Project/signInViewController.swift
+++ iOS Group Project/signInViewController.swift
@@ -10,9 +10,57 @@ import UIKit
 
 class signInViewController: UIViewController {
 
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var rememberMeSwitch: UISwitch!
     @IBOutlet weak var continueButton: UIButton!
+    
+    var username: String = ""
+    var password: String = ""
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
             // Do any additional setup after loading the view.
     }
+    
+    @IBAction func continueButtonTapped(_ sender: UIButton) {
+        guard let username = usernameTextField.text,
+              let password = passwordTextField.text
+        else {
+            return
+        }
+        
+        // Check username and password
+        if username == "admin" && password == "password" {
+            print("Sign in successful")
+            
+            // Check remember me switch is on
+            if rememberMeSwitch.isOn {
+                UserDefaults.standard.set(username, forKey: "username")
+                UserDefaults.standard.set(password, forKey: "password")
+            } else {
+                // Clear saved username and password
+                UserDefaults.standard.removeObject(forKey: "username")
+                UserDefaults.standard.removeObject(forKey: "password")
+            }
+        } else {
+            print("Incorrect username or password")
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Load saved username and password
+        if let username = UserDefaults.standard.string(forKey: "username"),
+           let password = UserDefaults.standard.string(forKey: "password") {
+            usernameTextField.text = username
+            passwordTextField.text = password
+            rememberMeSwitch.isOn = true
+        } else {
+            rememberMeSwitch.isOn = false
+        }
+    }
+    
 }
--- iOS Group Project/signUpViewController.swift
+++ iOS Group Project/signUpViewController.swift
@@ -10,9 +10,12 @@ import UIKit
 
 class signUpViewController: UIViewController {
 
-    
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var continueButton: UIButton!
     
+    var username: String = ""
+    var password: String = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,5 +23,28 @@ class signUpViewController: UIViewController {
         continueButton.layer.cornerRadius = 10
         continueButton.layer.masksToBounds = true
             // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func continueButtonTapped(_ sender: UIButton) {
+        guard let username = usernameTextField.text,
+              let password = passwordTextField.text else {
+            return
+        }
+        
+        if username.isEmpty || password.isEmpty {
+            let alertController = UIAlertController(title: "Error", message: "Please fill in all fields!", preferredStyle: .alert)
+            alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            present(alertController, animated: true, completion: nil)
+            return
+        }
+        
+        print("Username: \(username)")
+        print("Password: \(password)")
+        
+        let alertController = UIAlertController(title: "Success", message: "Account created!", preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "OK.", style: .default, handler: { (_) in
+        }))
+        present(alertController, animated: true, completion: nil)
     }
 }
