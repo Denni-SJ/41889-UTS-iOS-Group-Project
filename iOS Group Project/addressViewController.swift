3ce7ca7f02b335af3347e7ee3ba49dca0a742a2b
louistsou <louis6251@gmail.com>
2023-05-21 21:11:17 +1000

    update addressviewcontroller

--- iOS Group Project/addressViewController.swift
+++ iOS Group Project/addressViewController.swift
@@ -29,62 +29,50 @@ class addressViewController: UIViewController {
         errorText.isHidden = true
     }
     @IBAction func submitButtonPressed(_ sender: UIButton) {
-        if self.firstNameText.text != nil {
-            newAccount.Fname = firstNameText.text!
-        }
-        else {
+        if self.firstNameText.text == nil {
             errorText.isHidden = false
         }
 
-        if self.lastNameText.text != nil {
-            newAccount.Fname = firstNameText.text!
-        }
-        else {
+        if self.lastNameText.text == nil {
             errorText.isHidden = false
         }
 
-        if self.addressLine1Text.text != nil {
+        if self.addressLine1Text.text == nil {
+            errorText.isHidden = false
+        }
+
+        if self.addressLine2Text.text == nil {
+
+            errorText.isHidden = false
+        }
+
+        if self.cityText.text == nil {
+            errorText.isHidden = false
+        }
+
+        if self.postalCodeText.text == nil {
+            errorText.isHidden = false
+        }
+
+        if self.phoneNumberText.text == nil {
+
+            errorText.isHidden = false
+        }
+
+        if self.deliveryInstructText.text == nil {
+            errorText.isHidden = false
+        }
+        else {
+            newAccount.Fname = firstNameText.text!
+            newAccount.Fname = firstNameText.text!
             newAccount.Lname = lastNameText.text!
-        }
-        else {
-            errorText.isHidden = false
-        }
-
-        if self.addressLine2Text.text != nil {
             newAccount.address = addressLine1Text.text!
-        }
-        else {
-            errorText.isHidden = false
-        }
-
-        if self.cityText.text != nil {
             newAccount.address2 = addressLine2Text.text!
-        }
-        else {
-            errorText.isHidden = false
-        }
-
-        if self.postalCodeText.text != nil {
             newAccount.city = cityText.text!
-        }
-        else {
-            errorText.isHidden = false
-        }
-
-        if self.phoneNumberText.text != nil {
             newAccount.phone = Int(phoneNumberText.text!)!
-        }
-        else {
-            errorText.isHidden = false
-        }
-
-        if self.deliveryInstructText.text != nil {
             newAccount.deliveryInst = deliveryInstructText.text!
-        }
-        else {
-            errorText.isHidden = false
+            UserDefaults.standard.set(newAccount, forKey: userEmail)
         }
         
-        UserDefaults.standard.set(newAccount, forKey: userEmail)
     }
 }
