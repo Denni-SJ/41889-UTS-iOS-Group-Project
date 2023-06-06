d2d091449865ae7218fdbe58b6735bbe267c988b
louistsou <louis6251@gmail.com>
2023-06-06 21:25:55 +1000

    postal validation

--- iOS Group Project/postalCodeViewController.swift
+++ iOS Group Project/postalCodeViewController.swift
@@ -12,12 +12,43 @@ class postalCodeViewController: UIViewController {
     
     
     @IBOutlet weak var postalCodeTextField: UITextField!
-
+    @IBOutlet weak var validLabel: UILabel!
+    @IBOutlet weak var submitButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        validLabel.isHidden = true
         postalCodeTextField.layer.cornerRadius = 10
         postalCodeTextField.layer.masksToBounds = true
             // Do any additional setup after loading the view.
     }
+    
+    func isValid(text: String) -> Bool {
+        let range = NSRange(location: 0, length: 4)
+        let regex = try! NSRegularExpression(pattern: "[0-9]{4}")
+        guard !text.isEmpty else {
+            validLabel.isHidden = false
+            return false
+        }
+        if (regex.firstMatch(in: text, range: range) != nil) {
+            return true
+        }
+        else {
+            validLabel.isHidden = false
+            return false
+        }
+    }
+    
+    @IBAction func submit(_ sender: UIButton) {
+        if(isValid(text: postalCodeTextField.text!)) {
+           let vc = self.storyboard?.instantiateViewController(withIdentifier: "signUpViewController") as! signUpViewController
+            self.navigationController?.pushViewController(vc, animated: true)
+            
+        }
+        else {
+            validLabel.isHidden = false
+        }
+            
+    }
+    
+    
 }
