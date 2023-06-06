bb7ef33e9b9112924b7f3b4dc1078b76329534ab
louistsou <louis6251@gmail.com>
2023-06-06 21:44:30 +1000

    update postal validation

--- iOS Group Project/postalCodeViewController.swift
+++ iOS Group Project/postalCodeViewController.swift
@@ -9,8 +9,9 @@ import Foundation
 import UIKit
 
 class postalCodeViewController: UIViewController {
-    
-    
+
+
+
     @IBOutlet weak var postalCodeTextField: UITextField!
     @IBOutlet weak var validLabel: UILabel!
     @IBOutlet weak var submitButton: UIButton!
@@ -19,12 +20,12 @@ class postalCodeViewController: UIViewController {
         validLabel.isHidden = true
         postalCodeTextField.layer.cornerRadius = 10
         postalCodeTextField.layer.masksToBounds = true
-            // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view.
     }
-    
+
     func isValid(text: String) -> Bool {
-        let range = NSRange(location: 0, length: 4)
-        let regex = try! NSRegularExpression(pattern: "[0-9]{4}")
+        let range = NSRange(location: 0, length: postalCodeTextField.text!.count)
+        let regex = try! NSRegularExpression(pattern: "^[0-9]{4}")
         guard !text.isEmpty else {
             validLabel.isHidden = false
             return false
@@ -37,18 +38,26 @@ class postalCodeViewController: UIViewController {
             return false
         }
     }
-    
+
     @IBAction func submit(_ sender: UIButton) {
         if(isValid(text: postalCodeTextField.text!)) {
-           let vc = self.storyboard?.instantiateViewController(withIdentifier: "signUpViewController") as! signUpViewController
+            let vc = self.storyboard?.instantiateViewController(withIdentifier: "signUpViewController") as! signUpViewController
             self.navigationController?.pushViewController(vc, animated: true)
-            
+
         }
         else {
             validLabel.isHidden = false
+            postalCodeTextField.text = ""
         }
-            
     }
-    
-    
+}
+
+extension NSRegularExpression {
+    convenience init(_ pattern: String) {
+        do {
+            try self.init(pattern: pattern)
+        } catch {
+            preconditionFailure("Illegal regular expression: \(pattern).")
+        }
+    }
 }
